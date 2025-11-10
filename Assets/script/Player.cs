391e4a58ec66e9ec9f882f058e61231a21391bf3
NolanPestre <npestre@gaming.tech>
2025-11-10 20:22:06 +0100

    ADD reuperer loot + effet de briance a termier

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -1,8 +1,19 @@
+﻿using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
+    private SpriteRenderer sr;
+    private Color Originalcolor;
+    private bool IsPlayerNear=false;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        if (sr != null)
+            Originalcolor = sr.color;
+    }
 
     void Update()
     {
@@ -15,11 +26,11 @@ public class PlayerMovement : MonoBehaviour
         float moveY = 0f;
 
 
-        if (Input.GetKey(KeyCode.Z))
+        if (Input.GetKey(KeyCode.W))
             moveY = 1f;
         if (Input.GetKey(KeyCode.S))
             moveY = -1f;
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.A))
             moveX = -1f;
         if (Input.GetKey(KeyCode.D))
             moveX = 1f;
@@ -27,5 +38,43 @@ public class PlayerMovement : MonoBehaviour
         Vector2 move = new Vector2(moveX, moveY).normalized;
 
         transform.position += (Vector3)(move * speed * Time.deltaTime);
+    }
+
+    void HandlingHightLight()
+    {
+        if (sr == null)
+        {
+            return;
+        }
+        if (IsPlayerNear)
+        {
+            float alpha = Mathf.PingPong(Time.time, 1f);
+            sr.color = new Color(1f, 1f, alpha);
+        }
+        else
+        {
+            sr.color = Originalcolor;
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("loot"))
+            {
+                Debug.Log("Le joueur a touché une arme !");
+                IsPlayerNear = true;
+            }
+
+        }
+
+        void OnTriggerStay2D(Collider2D other)
+        {
+            if (other.CompareTag("loot"))
+            {
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    Destroy(other.gameObject);
+                }
+            }
+        }
     }
 }
